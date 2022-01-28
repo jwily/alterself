1c8d84d682f96848f12ace27f66f98663b15669f
John J. Lee <johnlee1120@gmail.com>
2022-01-27 19:30:23 -0800

    Migration and seeds

---
+++ app/seeds/pfts.py
@@ -0,0 +1,26 @@
+from app.models import db, Feature, Proficiency
+
+
+def seed_pfts():
+
+    zex1 = Proficiency(
+        char_id=1,
+        name='Languages',
+        description='Common'
+    )
+
+    zex2 = Feature(
+        char_id=1,
+        name='Wild Shape'
+    )
+
+    db.session.add(zex1)
+    db.session.add(zex2)
+
+    db.session.commit()
+
+
+def undo_pfts():
+    db.session.execute('TRUNCATE items RESTART IDENTITY CASCADE;')
+    db.session.execute('TRUNCATE items RESTART IDENTITY CASCADE;')
+    db.session.commit()
