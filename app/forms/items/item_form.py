3eae2f274bc76bb89be535d5fd4cbcc671b6d2c2
John J. Lee <johnlee1120@gmail.com>
2022-01-29 14:01:48 -0800

    Fix wtforms imports

--- app/forms/items/item_form.py
+++ app/forms/items/item_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
-from wtforms.validators import DataRequired, NumberRange
+from wtforms.validators import DataRequired, NumberRange, Length
 
 
 class ItemForm(FlaskForm):
