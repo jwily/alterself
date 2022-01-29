f527df5af95d196dfcae2c8c2b2717729ca104dc
John J. Lee <johnlee1120@gmail.com>
2022-01-29 13:49:56 -0800

    Remove last name from table

--- app/models/user.py
+++ app/models/user.py
@@ -9,7 +9,6 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), nullable=False, unique=True)
     first_name = db.Column(db.String(40), nullable=False)
-    last_name = db.Column(db.String(40), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
@@ -30,6 +29,6 @@ class User(db.Model, UserMixin):
         return {
             'id': self.id,
             'username': self.username,
-            'name': self.first_name + ' ' + self.last_name[0] + '.',
+            'name': self.first_name,
             'email': self.email,
         }
