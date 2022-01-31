1de0478ee9e23129defd6f35263912c40a404420
John J. Lee <johnlee1120@gmail.com>
2022-01-30 19:59:07 -0800

    Remove home due to style conflicts and intro done

--- react-app/src/components/Welcome.js
+++ react-app/src/components/Welcome.js
@@ -6,7 +6,7 @@ import {
     faDAndD,
 } from '@fortawesome/free-brands-svg-icons';
 
-const Container = styled.div`
+const WelcomePage = styled.div`
 
     height: 100%;
     width: 100%;
@@ -15,39 +15,89 @@ const Container = styled.div`
     flex-direction: column;
     align-items: center;
 
-    margin-top: 5rem;
-
     h1 {
         font-size: 5rem;
         filter: drop-shadow(5px 5px 5px rgba(0, 0, 0, .75));
         font-family: 'Cormorant SC', serif;
     }
 
-    h2 {
-        position: fixed;
-        top: 5rem;
+    div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .dnd-logo {
+        position: absolute;
+        top: 7rem;
         font-size: 10rem;
         z-index: -1;
         color: rgb(186, 6, 22);
     }
 
     .welcome-title {
-        display: flex;
-        justify-content: center;
+        padding: 7.5rem;
+        width: 60rem;
+    }
+
+    .welcome-intro {
+        background-color: rgb(25, 24, 23);
+        width: 100%;
+        padding: 2.5rem;
+    }
+
+    p {
+        max-width: 45rem;
+        font-size: 1rem;
+        line-height: 1.5rem;
+        margin-bottom: 1rem;
+        font-size: 1.15rem;
+        padding-left: 1rem;
+    }
+
+    .welcome-info {
+        width: 100%;
+        padding: 2.5rem;
+    }
+
+    h2 {
+        font-size: 2rem;
+        font-family: 'Cormorant SC', serif;
+        filter: drop-shadow(5px 5px 5px rgba(0, 0, 0, .75));
+        margin-bottom: 2rem;
     }
 `
 
 const Welcome = () => {
     return (
-        <Container>
+        <WelcomePage>
             <div className="welcome-title">
                 <h1>A L T E R S E L F</h1>
-                <h2><FontAwesomeIcon icon={faDAndD} /></h2>
+                <div className="dnd-logo"><FontAwesomeIcon icon={faDAndD} /></div>
             </div>
             <div className="welcome-intro">
-
+                <h2>What is tabletop role-playing?</h2>
+                <p>
+                    It's a common childhood experience, regardless of where we each call home, to set alight the imagination and put ourselves in the shoes of another: a star athlete, a boundless caped crusader, or perhaps even our favorite character from one of Tolkien's timeless epics.
+                </p>
+                <p>
+                    Tabletop role-playing takes this universal form of play and weaves into it a set of rules that determines everything from how high we might fly to the might of the dragons we vanquish in our dreams.
+                </p>
+                <p>
+                    Now, on the surface, adding rules to make-believe might seem entirely like defeating the purpose.
+                </p>
+                <p>
+                    However, when we reflect upon the relationship between creativity and limitation, we can that it's precisely the existence of such rules that would invite a group of dreamers, sitting together round a table, to bring all of their daring, cunning, and heart to bear in surmounting the obstacles placed before them.
+                </p>
             </div>
-        </Container>
+            <div className="welcome-info">
+                <h2>What is Alter Self?</h2>
+                <p>
+
+                </p>
+            </div>
+        </WelcomePage>
     )
 };
 
