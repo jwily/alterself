72066943cc1a3c76164f9e62b86e4a63b5bbc757
John J. Lee <johnlee1120@gmail.com>
2022-02-10 20:39:26 -0800

    Tweak margin

--- react-app/src/components/characters/Vitals.js
+++ react-app/src/components/characters/Vitals.js
@@ -103,7 +103,7 @@ const LifeBar = styled.div`
     ${(props) => props.width > 50 && 'background-color: yellowgreen;'}
     ${(props) => props.width <= 50 && props.width > 25 && 'background-color: goldenrod;'}
     ${(props) => props.width <= 25 && 'background-color: firebrick;'}
-    transition: width .15s;
+    transition: all .15s;
     grid-area: 1 / 1;
 `
 
@@ -111,7 +111,7 @@ const ShieldBar = styled.div`
     width: ${(props) => props.width}%;
     height: .5rem;
     background-color: slateblue;
-    transition: width .15s;
+    transition: all .15s;
     grid-area: 1 / 1;
     justify-self: end;
 `
